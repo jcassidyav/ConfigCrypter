e2371a843a791f61c6d489c4ee877e39273d911e
kivers <kivers@avaeon.com>
2024-10-14 15:15:10 +0100

    Sign both

--- ConfigCrypter.Console/signDlls.ps1
+++ ConfigCrypter.Console/signDlls.ps1
@@ -22,6 +22,17 @@ if ($Configuration -eq "Release") {
        Set-AuthenticodeSignature -FilePath $signed.path -Certificate $cert -TimestampServer "http://timestamp.comodoca.com/authenticode"
     }
 	
+	cd $ProjectDir\bin\Release\net8.0
+	
+	$signed = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -ne "Valid"}
+    $notSigned = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -eq "Valid"}
+    $notSignedFileNames = Get-ChildItem -Path $notSigned.Path | Sort-Object | Select-Object -Unique -Property Name
+	
+	if($signed -ne $null)
+    {
+       Set-AuthenticodeSignature -FilePath $signed.path -Certificate $cert -TimestampServer "http://timestamp.comodoca.com/authenticode"
+    }
+	
 } else {
     Write-Output "Debug: Nothing signed!"
 }
