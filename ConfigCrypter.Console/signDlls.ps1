f348d18d03fff6126f66a0329bd75e1bbf484a1f
AVAEON\kivers <kivers@avaeon.com>
2024-10-15 11:55:14 +0100

    Update signing cert

--- ConfigCrypter.Console/signDlls.ps1
+++ ConfigCrypter.Console/signDlls.ps1
@@ -4,35 +4,51 @@
 # )
 $ProjectDir = $args[0]
 $Configuration = $args[1]
+$SolutionDir = $args[2]
+
+Write-Output "Solution Dir: $SolutionDir"
+
+function Sign-Files {
+    param (
+        [string]$DirectoryPath,
+        [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
+        [string]$TimestampServer = "http://timestamp.comodoca.com/authenticode"
+    )
+
+    # Change to the specified directory
+    cd $DirectoryPath
+
+    # Get files that are not signed
+    $signed = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-Object { Get-AuthenticodeSignature $_ } | Where-Object { $_.Status -ne "Valid" }
+    $notSigned = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-Object { Get-AuthenticodeSignature $_ } | Where-Object { $_.Status -eq "Valid" }
+    $notSignedFileNames = Get-ChildItem -Path $notSigned.Path | Sort-Object | Select-Object -Unique -Property Name
+
+    # Sign the files if there are any unsigned files
+    if ($signed -ne $null) {
+        Set-AuthenticodeSignature -FilePath $signed.Path -Certificate $Certificate -TimestampServer $TimestampServer
+    }
+}
 
 
 if ($Configuration -eq "Release") {
     
 	$cert = Get-ChildItem -Path Cert:\* -Recurse -CodeSigningCert
-	Write-Output "Signing output in $ProjectDir\bin\Release\net8.0"
+    $signLoc = "$ProjectDir\obj\Release\net8.0"
+	Write-Output "*****Signing output in $signLoc"
+	Sign-Files -DirectoryPath $signLoc -Certificate $cert
+
+    $signLoc = "$ProjectDir\bin\Release\net8.0"
+	Write-Output "*****Signing output in $signLoc"
+	Sign-Files -DirectoryPath $signLoc -Certificate $cert
+
+    $signLoc = "$SolutionDir\ConfigCrypter\bin\Release\net8.0"
+	Write-Output "*****Signing output in $signLoc"
+	Sign-Files -DirectoryPath $signLoc -Certificate $cert
+
 	
-	cd $ProjectDir\obj\Release\net8.0
 	
-	$signed = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -ne "Valid"}
-    $notSigned = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -eq "Valid"}
-    $notSignedFileNames = Get-ChildItem -Path $notSigned.Path | Sort-Object | Select-Object -Unique -Property Name
-	
-	if($signed -ne $null)
-    {
-       Set-AuthenticodeSignature -FilePath $signed.path -Certificate $cert -TimestampServer "http://timestamp.comodoca.com/authenticode"
-    }
-	
-	cd $ProjectDir\bin\Release\net8.0
-	
-	$signed = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -ne "Valid"}
-    $notSigned = Get-ChildItem -include ('*.dll', '*.exe') -Recurse | ForEach-object {Get-AuthenticodeSignature $_} | Where-Object {$_.status -eq "Valid"}
-    $notSignedFileNames = Get-ChildItem -Path $notSigned.Path | Sort-Object | Select-Object -Unique -Property Name
-	
-	if($signed -ne $null)
-    {
-       Set-AuthenticodeSignature -FilePath $signed.path -Certificate $cert -TimestampServer "http://timestamp.comodoca.com/authenticode"
-    }
 	
 } else {
     Write-Output "Debug: Nothing signed!"
 }
+
